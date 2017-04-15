483912f4b48d943f4dc7f31aff4cc3c6f06892fc
Xiaocheng Yang <xiaocheng.yang@berkeley.edu>
2017-04-14 20:09:44 -0700

    Drawing rectangles, Maps
    Initial attempt for drawing rectangles

--- Maps/MapLayers.java
+++ Maps/MapLayers.java
@@ -1,9 +1,62 @@
+package Maps;
 import javax.swing.*;
-import java.awt.*;
+//import java.awt.*;
 import java.awt.event.*;
+import java.util.List;
+import java.util.LinkedList;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 
-public class PizzaPlaceXY implements ActionListener{
+
+public class MapLayers implements ActionListener{
+	/* The name of book. */
+	String bookName;
+	/* The call number. */
+	String callNum;
+	
+	/* The list of FLOOR Objects. */
+	List<Floor> flrL;
+	/* The index for floor it the book is on. */
+	int flr; 
+	/* Width of each floor on the screen. */
+	int flr_w = 30;
+	/* Length of each floor on the screen. */
+	int flr_l = 10;
+	/* Gap between floors. */
+	int flr_g = 50;
+	
+	/* LinkedList of floor button. */
+	LinkedList<Rectangles> flrBt;
+	
+	
+	
+	/* The list of SECTION Objects. */
+	List secL;
+	/* The index for section in FLR the book is on. */
+	int sec;
+	
+	/* The list of SHELF Objects. */
+	List slfL;
+	/* The index for shelf in SEC the book is on. */
+	int slf;
+	
+	/* The list of COLUMN Objects. */
+	List colL;
+	/* The index for column in SLF the book is on. */
+	int col;
+	
+	/* !!! The number of rows in the column, since the row is an estimation.  */
+	int NumRows;
+	/* The index for row in COL the book is on. */
+	int row;
+	
+	
+	
+	
 	JTextField custName = null;
 	String name;
 	
@@ -33,17 +86,32 @@ public class PizzaPlaceXY implements ActionListener{
 	String summary = null;
 	
 	public static void main(String[] args) {
-		new PizzaPlaceXY();
+		LinkedList<Floor> qwa = new LinkedList(java.util.Arrays.asList(new Floor("dfs"), new Floor("bfs")));
+		
+		new MapLayers(qwa, 1);
 	}
 	
-	public PizzaPlaceXY(){
+	public MapLayers(List<Floor> fL, int f){
+		flrL = fL;
+		flr = f; 
 		JFrame totalFrame = new JFrame();
-		totalFrame.setTitle("Pizza Place");
+		totalFrame.setTitle("Libray Maps");
 		totalFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		JPanel totalPanel = new JPanel();
-		totalPanel.setLayout(new BoxLayout(totalPanel, BoxLayout.Y_AXIS));
+		
+		totalPanel.setLayout(new BorderLayout());
+
+		
+		
 		totalPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 		totalFrame.add(totalPanel);
+		
+		
+		
+		/* Floor Printout. */
+		
+		totalPanel.add(new Rectangles(fL, f),BorderLayout.CENTER);
+
 		
 		//Get Customer Name
 		JPanel GetCustNamePanel = new JPanel();
@@ -51,74 +119,91 @@ public class PizzaPlaceXY implements ActionListener{
 		custName = new JTextField(20);
 		custName.addActionListener(this);
 		GetCustNamePanel.add(custName);
-		totalPanel.add(GetCustNamePanel);
+		//totalPanel.add(GetCustNamePanel);
+
 		
-		//Pizza Size:
-		JPanel PiSi = new JPanel();
-		ButtonGroup PizzaSize = new ButtonGroup();
-		PizzaSize.add(Small);
-		PizzaSize.add(Medium);
-		PizzaSize.add(Large);
-		
-		PiSi.add(Small);
-		PiSi.add(Medium);
-		PiSi.add(Large);
-		totalPanel.add(PiSi);	
-		
-		//Type of Crust
-		JPanel CrTy = new JPanel();
-		CrTy.add(new JLabel("Crust Type: "));
-		CrTy.add(Crust);
-		totalPanel.add(CrTy);
-		
-		//Pizza Toppings
-		JPanel PiTo = new JPanel();
-		PiTo.add(new JLabel("Toppings: "));
-		ToppingList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
-		
-		
-		JScrollPane ToScroll = new JScrollPane(ToppingList, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, 
-				JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-		PiTo.add(ToScroll);
-		
-		PiTo.add(ToppingList);
-		totalPanel.add(PiTo);
-		
-		//extra items
-		JPanel ExItem = new JPanel();
-		ExItem.add(new JLabel("Extras: "));
-		ExItem.add(BreadSticks);
-		ExItem.add(Salad);
-		ExItem.add(Soda);
-		totalPanel.add(ExItem);
-		
-		//Customer Comments
-		JPanel CuCom = new JPanel();
-		CuCom.add(new JLabel("Order Comments: "));
-		customerComments = new JTextArea(5,20);
-		customerComments.setLineWrap(true);
-		JScrollPane cuComScroll = new JScrollPane(customerComments, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
-				JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-		CuCom.add(cuComScroll);
-		totalPanel.add(CuCom);
 		
 		//Place Order or Reset
-		JPanel Buttoms = new JPanel();
-		Buttoms.add(PlaceOrder);
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.add(PlaceOrder);
 		PlaceOrder.addActionListener(this);
-		Buttoms.add(ResetValues);
+		buttonPanel.add(ResetValues);
 		ResetValues.addActionListener(this);
-		totalPanel.add(Buttoms);
+		totalPanel.add(buttonPanel);
 		
-		totalFrame.pack();
+		
+		buttonPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
+		JButton clickmeButton = new JButton("Click Me");
+		buttonPanel.add(clickmeButton);
+		totalPanel.add(buttonPanel,BorderLayout.SOUTH);
+		
+		totalFrame.setSize(600, 400);;
 		totalFrame.setVisible(true);
 		
 		
 		
 	}
 	
+	
+	
+	public class Rectangles extends JPanel {
+		List<DisplayObj> L;
+		int index;
+		
+		
+		public Rectangles(List Lst, int indexGood) {
+			L = Lst;
+			index = indexGood;
+		}
+		
+		public void paintComponent(Graphics g) {
+		    super.paintComponent(g);
+		    
+		    Graphics2D g2d = (Graphics2D) g;
+
+		    
+		    //flrBt = new LinkedList <Rectangles>();
+			int curDepth = 10;
+			for (Floor flr_i : flrL){
+				g2d.drawRect(5, curDepth, 500, 500);
+				//flrBt.add(new Rectangles(curDepth, 10, flr_w, flr_l));
+				curDepth += (flr_l + flr_g);
+				System.out.println (curDepth);
+			}
+			
+			/* The target floor button. */
+			//targetFlrBt = flrBt.get(flr);
+			
+			/* Set the listener to listen to the button with the right floor. */
+			//targetFlrBt.addMouseListener((MouseListener) this);
+			
+			//for(Rectangles flr_c: flrBt){
+			//	totalFrame.add(flr_c);
+			//	FloorPanel.add(new JButton("sfk"));
+			//	System.out.println ("1");
+			//}
+			
+		    
+		    
+		    
+		    /*
+			g2d.setColor(new Color(212, 212, 212));
+		    g2d.drawRect(x, y, w, l);
+
+		    g2d.setColor(new Color(31, 21, 1));
+		    g2d.fillRect(x, y, w, l);
+		    */
+
+		  }
+	}
+	
+	
+	
+	
 	public void actionPerformed (ActionEvent event){
 		Object control = event.getSource();
+		
+		
 		//Customer Name
 		if (control == PlaceOrder){
 			name = custName.getText();
